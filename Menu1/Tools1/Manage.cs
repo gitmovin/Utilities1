4e1a3b78f1dfacadd25e0138133b24f3f219f556
GitMovinGuy <ron@ronpearl.com>
2020-10-10 13:30:08 -0400

    Fixed generation of valid paths

--- Menu1/Tools1/Manage.cs
+++ Menu1/Tools1/Manage.cs
@@ -17,23 +17,31 @@ namespace Utilities
 		//		public string startingSourceFolder = @"D:\";
 		//		public string startingSourceFolder = @"D:\SP_JD";
 		public string startingSourceFolder = @"C:\zz_TEMP_IN";
-		public string startingDestinationFolder = @"C:\zzz_TST\DEST\";
-		public int MaxFileIterations = 500000;
-		public int MaxPathIterations = 500000;
-		public int totalIterations = 1000000;
+		public string startingDestinationFolder = @"C:\zz_TEMP_OUT\";
+
+		///////////////////////////////		public string finalFileLocation
+
+
+		/*
+				 public int MaxFileIterations = 500000;
+				public int MaxPathIterations = 500000;
+				public int totalIterations = 1000000;
+		*/
+		public int MaxFileIterations = 500;
+		public int MaxPathIterations = 500;
+		public int totalIterations = 1000;
+
 		public bool printDetailAccepted = true;
 		public bool printDetailRejected = false;
 		public bool printDetailMoved = true;
+		public bool writeFilesToDisk = true;
 		public string logFileName_Accepted = @"C:\zzz_TST\LOG\logFile_Accepted.txt";
 		public string logFileName_Rejected = @"C:\zzz_TST\LOG\logFile_Rejected.txt";
 		public string logFileName_Moved = @"C:\zzz_TST\LOG\logFile_Moved.txt";
 		public string fullyQualifiedfileName;
-		public string fileName;
-		public string fullPathName;
 		public string pathNamePart1;
 		public string pathAndFileNamePart2;
-		public string movedPathName;
-//		public string[] filePathParts;
+		public string movedFileName;
 		public int currentFileIteration = 0;
 		public int currentPathIteration = 0;
 		public int totalAccepted = 0;
@@ -91,14 +99,14 @@ namespace Utilities
 				{
 					if (printDetailRejected)
 					{
-						Console.WriteLine("REJECTED: " + fileName + " ");
+						Console.WriteLine("REJECTED: " + sourceFileName + " ");
 
 					}
 					else
 					{
 						Console.Write("R");
 					}
-					File.AppendAllText(logFileName_Rejected, "REJECTED: " + fileName + " " + Environment.NewLine);
+					File.AppendAllText(logFileName_Rejected, "REJECTED: " + sourceFileName + " " + Environment.NewLine);
 					totalRejected++;
 					continue;
 				}
@@ -109,8 +117,8 @@ namespace Utilities
 					if (printDetailMoved)
 					{
 						Console.WriteLine("");
-						Console.WriteLine("MOVEDFRM: " + fileName + " ");
-						Console.WriteLine("MOVED TO: " + movedPathName + " ");
+						Console.WriteLine("MOVEDFRM: " + sourceFileName + " ");
+						Console.WriteLine("MOVED TO: " + movedFileName + " ");
 						Console.WriteLine("");
 					}
 					else
@@ -118,9 +126,36 @@ namespace Utilities
 						Console.Write("M");
 					}
 
-					File.AppendAllText(logFileName_Moved, "MOVEDFROM: " + fileName + " " + Environment.NewLine);
-					File.AppendAllText(logFileName_Moved, "  MOVEDTO: " + movedPathName + " " + Environment.NewLine + Environment.NewLine);
-				}
+					File.AppendAllText(logFileName_Moved, "MOVEDFROM: " + sourceFileName + " " + Environment.NewLine);
+					File.AppendAllText(logFileName_Moved, "  MOVEDTO: " + movedFileName + " " + Environment.NewLine + Environment.NewLine);
+
+                    //
+                    // Start of section that copies files to diskd
+                    //
+/*
+                    if (writeFilesToDisk)
+                    {
+						// 
+						System.IO.Directory.CreateDirectory(movedFileName);
+					}
+
+					//
+					// End of section that copies files to disk
+					//
+
+					string dosCommand = "\"" + sourceFolderName + "\"";
+
+					
+					FileInfo fInfo = new FileInfo(dosCommand);
+					if (fInfo.Exists)
+					{
+						Console.WriteLine("File exits");
+					}
+                    else
+                    {
+						Console.WriteLine("File does not exist");
+                    }
+*/				}
                 else
                 {
 					// File was accepted but not moved
@@ -128,13 +163,13 @@ namespace Utilities
 					if (printDetailAccepted)
                     {
 
-						Console.WriteLine("ACCEPTED: " + fileName + " ");
+						Console.WriteLine("ACCEPTED: " + sourceFileName + " ");
 					}
 					else
 					{
 						Console.Write("A");
 					}
-					File.AppendAllText(logFileName_Accepted, "ACCEPTED: " + fileName + " " + Environment.NewLine);
+					File.AppendAllText(logFileName_Accepted, "ACCEPTED: " + sourceFileName + " " + Environment.NewLine);
 				}
 
 
@@ -142,6 +177,7 @@ namespace Utilities
 			Console.WriteLine("");
 			Console.WriteLine("Total Accepted: " + totalAccepted);
 			Console.WriteLine("Total Rejected: " + totalRejected);
+			Console.WriteLine("Total Moved: " + totalMoved);
 		}
 
 
@@ -155,8 +191,6 @@ namespace Utilities
 				{
 					// get next file name, decrement array pointer and return
 					sourceFileName = sourceFileNameList[--sourceFileNameListCount];
-					fullPathName = sourceFileName; // ////////////////////////////////  FIX THIS ///////////////////
-					fileName = sourceFileName; // ////////////////////////////////  FIX THIS ///////////////////
 
 					return true;
 				}
@@ -200,64 +234,64 @@ namespace Utilities
 			if (currentFileIteration >= MaxFileIterations) return false;
 			if (currentPathIteration >= MaxPathIterations) return false;
 
-			if (fullPathName.Contains("RECYCLE.BIN")) return false;
-			if (fullPathName.Contains(@"\from 1MM\Users\margaret\Documents\_R")) return false;
-			if (fullPathName.Contains(@"\from 1MM\Users\margaret\Documents\_M\people\RGP")) return false;
-			if (fullPathName.Contains(@"\from 3MBPro\Users\ronpearl\_M")) return false;
-			if (fullPathName.Contains(@"untitled folder")) return false;
-			if (fullPathName.Contains(@"New folder")) return false;
-			if (fullPathName.Contains(@"\.")) return false;
-			if (fullPathName.Contains(@"\from 2MBook\Users\apple\Library")) return false;
-			if (fullPathName.Contains(@"\from 2MBook\Users\apple\src\ltcadm")) return false;
-			if (fullPathName.Contains(@"\from 2MBook\Users\apple\src\ltcuat")) return false;
-			if (fullPathName.Contains(@"\from 2MBook\Users\apple\tmp")) return false;
-			if (fullPathName.Contains(@"\Address Book -")) return false;
-			if (fullPathName.Contains(@"\ScanSnap\")) return false;
-			if (fullPathName.Contains(@"z_backups\")) return false;
-			if (fullPathName.Contains(@"\z_Archive")) return false;
-			if (fullPathName.Contains(@"trash")) return false;
-			if (fullPathName.Contains(@"MasterFileUpdates")) return false;
-			if (fullPathName.Contains(@"\Metadata\")) return false;
-			if (fullPathName.Contains(@"1Password")) return false;
-			if (fullPathName.Contains(@"- DELETE -")) return false;
-			if (fullPathName.Contains(@"Django")) return false;
-			if (fullPathName.Contains(@"\temp\")) return false;
-			if (fileName.Contains(@".DS_Store")) return false;
-			if (fileName[0] == '.') return false;
+			if (sourceFileName.Contains("RECYCLE.BIN")) return false;
+			if (sourceFileName.Contains(@"\from 1MM\Users\margaret\Documents\_R")) return false;
+			if (sourceFileName.Contains(@"\from 1MM\Users\margaret\Documents\_R_archive")) return false;
+			if (sourceFileName.Contains(@"\from 1MM\Users\margaret\Documents\_M\people\RGP")) return false;
+			if (sourceFileName.Contains(@"\from 3MBPro\Users\ronpearl\_M")) return false;
+			if (sourceFileName.Contains(@"untitled folder")) return false;
+			if (sourceFileName.Contains(@"New folder")) return false;
+			if (sourceFileName.Contains(@"\.")) return false;
+			if (sourceFileName.Contains(@"\from 2MBook\Users\apple\Library")) return false;
+			if (sourceFileName.Contains(@"\from 2MBook\Users\apple\src\ltcadm")) return false;
+			if (sourceFileName.Contains(@"\from 2MBook\Users\apple\src\ltcuat")) return false;
+			if (sourceFileName.Contains(@"\from 2MBook\Users\apple\tmp")) return false;
+			if (sourceFileName.Contains(@"\Address Book -")) return false;
+			if (sourceFileName.Contains(@"\ScanSnap\")) return false;
+			if (sourceFileName.Contains(@"z_backups\")) return false;
+			if (sourceFileName.Contains(@"\z_Archive")) return false;
+			if (sourceFileName.Contains(@"trash")) return false;
+			if (sourceFileName.Contains(@"MasterFileUpdates")) return false;
+			if (sourceFileName.Contains(@"\Metadata\")) return false;
+			if (sourceFileName.Contains(@"1Password")) return false;
+			if (sourceFileName.Contains(@"- DELETE -")) return false;
+			if (sourceFileName.Contains(@"Django")) return false;
+			if (sourceFileName.Contains(@"\temp\")) return false;
+			if (sourceFileName.Contains(@".DS_Store")) return false;
+			if (sourceFileName[0] == '.') return false;
 			return true;
 		}
 		private bool moveFile()
         {
 			// Check the patterns against full path & file names, 
 			// generate destination path & file name.
-			if (buildMovePath(@"\_emergency\"))	return true;
-			if (buildMovePath(@"\music\")) return true;
-			if (buildMovePath(@"\Music\")) return true;
-			if (buildMovePath(@"\things\")) return true;
-			if (buildMovePath(@"\Things\")) return true;
-			if (buildMovePath(@"\people\")) return true;
-			if (buildMovePath(@"\People\")) return true;
-			if (buildMovePath(@"\places\")) return true;
-			if (buildMovePath(@"\events\")) return true;
-			if (buildMovePath(@"\Pictures\")) return true;
-			if (buildMovePath(@"\photos\")) return true;
-			if (buildMovePath(@"\Photos\")) return true;
-			if (buildMovePath(@"\orgs\")) return true;
-			if (buildMovePath(@"\Organizations\")) return true;
-			if (buildMovePath(@"\_PEARL\")) return true;
-			if (buildMovePath(@"\Seabolt\")) return true;
-			if (buildMovePath(@"\neighborhoods\")) return true;
-			if (buildMovePath(@"\cdsir\")) return true;
-			if (buildMovePath(@"\Desktop\")) return true;
-			if (buildMovePath(@"\zip\")) return true;
-			if (buildMovePath(@"\src\")) return true;
-			if (buildMovePath(@"\Src\")) return true;
-			///			if (buildMovePath(@"zz")) return true;
+			if (buildMovePath(@"_emergency\"))	return true;
+			if (buildMovePath(@"music\")) return true;
+			if (buildMovePath(@"Music\")) return true;
+			if (buildMovePath(@"things\")) return true;
+			if (buildMovePath(@"Things\")) return true;
+			if (buildMovePath(@"people\")) return true;
+			if (buildMovePath(@"People\")) return true;
+			if (buildMovePath(@"places\")) return true;
+			if (buildMovePath(@"events\")) return true;
+			if (buildMovePath(@"Pictures\")) return true;
+			if (buildMovePath(@"photos\")) return true;
+			if (buildMovePath(@"Photos\")) return true;
+			if (buildMovePath(@"orgs\")) return true;
+			if (buildMovePath(@"Organizations\")) return true;
+			if (buildMovePath(@"_PEARL\")) return true;
+			if (buildMovePath(@"Seabolt\")) return true;
+			if (buildMovePath(@"neighborhoods\")) return true;
+			if (buildMovePath(@"cdsir\")) return true;
+			if (buildMovePath(@"Desktop\")) return true;
+			if (buildMovePath(@"zip\")) return true;
+			if (buildMovePath(@"src\")) return true;
+			if (buildMovePath(@"Src\")) return true;
 			return false;
 		}
         private bool buildMovePath(string pattern)
         {
-            if (!fullPathName.Contains(pattern))
+            if (!sourceFileName.Contains(pattern))
             {
 				// The fully qualified path and file name do not contain the pattern.
 				return false;
@@ -265,57 +299,66 @@ namespace Utilities
 			// The path and file name do contain the pattern.
 			// Build to new path and file name after calculating the
 			// characters to chop out of the middle of the original path name
-			string tempString = startingDestinationFolder + fullPathName;
+			string tempString = startingDestinationFolder + sourceFileName;
 			int cutStart = startingDestinationFolder.Length;
 			int cutEnd = tempString.IndexOf(pattern);
 
-			// cut out the old portion of the path that will not be used after the move
-			string tempString2 = tempString.Remove(cutStart, cutEnd - cutStart - 1);
-
-			// EXPERIMENTAL CODE
-			// Add a code representing the source of the data right after the pattern folder
-
-			if (fullPathName.Contains("3MBPro"))
-			{
-				movedPathName = tempString2.Insert(cutStart + pattern.Length + 1, @"3MBPro\");
-			} else
-			if (fullPathName.Contains("2MBook"))
-			{
-				movedPathName = tempString2.Insert(cutStart + pattern.Length + 1, @"2MBook\");
-			} else
-
-			if (fullPathName.Contains("1MM"))
-			{
-				movedPathName = tempString2.Insert(cutStart + pattern.Length + 1, @"1MM\");
-			}
 
 
 
 
-			/*
-			 * if (insertDataSource("3MBPro")) return;
-						if (insertDataSource("2MBook")) return;
-						if (insertDataSource("1MM")) return;
-						if (insertDataSource("0")) return;
-			*/
-			//			if (fullPathName.Contains("3MBPro")) { movedPathName = tempString2.Insert(cutStart + pattern.Length + 1, "3MBPro"); return;
-			//			if (fullPathName.Contains("3MBPro")) { movedPathName = tempString2.Insert(cutStart + pattern.Length + 1, "3MBPro"); return;
-
-
-
-
-
-
-			// What's left is the new path name to which the file will be moved
-			return true;
-       }
-/*		private bool insertDataSource(int atLocation, string dataSource)
-        {
-			if (fullPathName.Contains(dataSource))
+			// loop backwards through temporary string until "\" is found.
+			// This allows patterns to not start with "\".
+			while (cutEnd > 0)
             {
-				
+				if (tempString[cutEnd] == '\\')
+				{
+					break;
+				}
+				cutEnd--;
             }
+
+
+
+
+			// cut out the old portion of the path that will not be used after the move
+			string tempString2 = tempString.Remove(cutStart, cutEnd - cutStart + 1);
+
+			if (sourceFileName.Contains("3MBPro"))
+			{
+				movedFileName = tempString2.Insert(cutStart + pattern.Length + 1, @"3MBPro\");
+			}
+			else
+			if (sourceFileName.Contains("2MBook"))
+			{
+				movedFileName = tempString2.Insert(cutStart + pattern.Length + 1, @"2MBook\");
+			}
+			else
+
+			if (sourceFileName.Contains("1MM"))
+			{
+				movedFileName = tempString2.Insert(cutStart + pattern.Length + 1, @"1MM\");
+			}
+			else
+			{
+				movedFileName = tempString2.Insert(cutStart + pattern.Length + 1, @"0_other\");
+			}
+			return true;
         }
+/*
+		private bool copyFile()
+        {
+			// copies file from "sourceFileName" to "movedFileName"
+
+			// See whether source file still exists
+
+
+			// See whether movedFileName (destination) file exists
+
+
+			// execute the windows copy command (AFTER DEALING WITH THE CREATION OF THE FOLDER WITHIN WHICH THE FILE IS TO BE COPIED)
+			return true;
+		}
 */
 	}
 }
